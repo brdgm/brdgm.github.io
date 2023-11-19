40aa10eebf42a66a493618ebd109ce3930aa3e61
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-11-19 19:15:45 +0100

    fix menu behavior

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -1,4 +1,5 @@
-document.querySelector('.menuOpener').addEventListener('click', event => {
+function toggleSidebar(event) {
   event.preventDefault();
   document.querySelector('.sidebar').classList.toggle('open');
-});
+}
+document.body.querySelector('.menuOpener').addEventListener('click', toggleSidebar);
